52eb29d28b2fde6ac6a434f42987274489d776d6
Arik Jones <makingme@gmail.com>
2009-05-18 15:40:34 -0500

    Removed Certify (yet again because it's still super buggy). Went back with authlogic, going to roll my own account+subdomain+signup scheme with authlogic. Renamed admin_tabs.js to tabs.js. [next: Create basic "public" signup pages.]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  
   include Kind
-  
   helper :all
   # helper_method :current_user_session, :current_user
   filter_parameter_logging :password, :password_confirmation
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,37 +5,40 @@ class User < ActiveRecord::Base
   has_one :account
   has_many :entries
   
-  # def self.register(user_params, account_params, host = 'kindmanage.dev')
-  #   user_params ||= {}
-  #   account_params ||= {}
-  # 
-  #   # you can choose to provide a password field, or have a password generated
-  #   # we'll default it here if you haven't provided a field
-  #   user_params[:password] ||= encrypt(Time.now)[0..8]
-  # 
-  #   account = ::Account.new(account_params)
-  #   user = ::User.new(user_params)
-  # 
-  #   begin
-  #     account.rollback_active_record_state! do
-  #       transaction do
-  #         # save the account
-  #         account.save!
-  #         # assign the user to it
-  #         user.account_id = account.id
-  #         # save the user
-  #         if user.save!
-  #           # if saved: send email notification of success
-  #           ::Notifier.deliver_registration(user, account, user_params[:password], host)
-  #         end
-  #       end
-  #     end
-  #   rescue ActiveRecord::RecordInvalid
-  #     # do nothing
-  #   end
-  # 
-  #   # return the pair as an array
-  #   [user, account]
-  # end
+  before_save :create_account
+        
+    
+  def self.register(user_params, account_params, host = 'kindmanage.dev')
+    user_params ||= {}
+    account_params ||= {}
+  
+    # you can choose to provide a password field, or have a password generated
+    # we'll default it here if you haven't provided a field
+    user_params[:password] ||= encrypt(Time.now)[0..8]
+  
+    account = Account.new(account_params)
+    user = User.new(user_params)
+  
+    begin
+      account.rollback_active_record_state! do
+        transaction do
+          # save the account
+          account.save!
+          # assign the user to it
+          user.account_id = account.id
+          # save the user
+          if user.save!
+            # if saved: send email notification of success
+            ::Notifier.deliver_registration(user, account, user_params[:password], host)
+          end
+        end
+      end
+    rescue ActiveRecord::RecordInvalid
+      # do nothing
+    end
+  
+    # return the pair as an array
+    [user, account]
+  end
   
 end
